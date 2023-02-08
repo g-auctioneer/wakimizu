8654c595d61d20fe969768d80eaef5282830ceb2
g-auctioneer <93477932+g-auctioneer@users.noreply.github.com>
2023-02-09 01:00:43 +0900

    Get selected value.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,23 @@
-// import { useState } from "react";
+import { useState } from "react";
 import prefs from "../utils/define";
 
 const Index = () => {
+  const [pref, setPref] = useState("北海道");
+
+  const fetchWakimizu = () => {
+    // exec
+  }
+
   return (
     <div>
-      <select>
-        {prefs.map(pref => {
-          return <option value={pref}>{pref}</option>
+      <select onChange={e => setPref(e.target.value)}>
+        {prefs.map((pref, index) => {
+          return <option value={pref} key={index}>{pref}</option>
         })}
       </select>
+      <button onClick={fetchWakimizu}>
+        検索
+      </button>
     </div>
   );
 }
