a2a3e99576e4bce62df9fe5344768dde728850b6
g-auctioneer <93477932+g-auctioneer@users.noreply.github.com>
2023-02-09 22:57:48 +0900

    Fetch data with wakimizu-API.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import prefs from "../utils/define";
 
 const Index = () => {
   const [pref, setPref] = useState("北海道");
+  const [wakimizu, setWakimizu] = useState({});
 
-  const fetchWakimizu = () => {
-    // exec
+  const fetchWakimizu = async () => {
+    const result = await fetch(`https://livlog.xyz/webapi/springWater?q=${pref}`)
+      .then(res => res.json());
+    setWakimizu(result.result);
   }
 
+  useEffect(() => {
+    if (Object.keys(wakimizu).length) {
+      // not empty
+    }
+  }, [wakimizu]);
+
   return (
-    <div>
-      <select onChange={e => setPref(e.target.value)}>
-        {prefs.map((pref, index) => {
-          return <option value={pref} key={index}>{pref}</option>
-        })}
-      </select>
-      <button onClick={fetchWakimizu}>
-        検索
-      </button>
-    </div>
+    <>
+      <div>
+        <select onChange={e => setPref(e.target.value)}>
+          {prefs.map((pref, index) => {
+            return <option value={pref} key={index}>{pref}</option>
+          })}
+        </select>
+        <button onClick={fetchWakimizu}>
+          検索
+        </button>
+      </div>
+      <div>
+      </div>
+    </>
   );
 }
 
